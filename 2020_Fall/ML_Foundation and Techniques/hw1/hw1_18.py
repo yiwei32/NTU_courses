266b62277206acb729aca0287aea948c01182503
yiwei32 <63040592+yiwei32@users.noreply.github.com>
2021-03-30 12:06:32 +0800

    Add files via upload

---
+++ 2020_Fall/ML_Foundation and Techniques/hw1/hw1_18.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[255]:
+
+
+import pandas as pd
+import numpy as np
+import random as rdm
+from statistics import median
+
+
+# In[256]:
+
+
+mydata = pd.read_csv("https://www.csie.ntu.edu.tw/~htlin/course/ml20fall/hw1/hw1_train.dat" , sep="\s+", header = None)
+
+
+# In[257]:
+
+
+def sign(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
+
+
+# In[267]:
+
+
+N = 100
+data = mydata.to_numpy()
+x = np.ones([N,11]) * 10
+ans_y = np.empty(N)
+for i in range(N):
+    ans_y[i] = data[i,10]
+    for j in range(10):
+        x[i,j+1] = data[i,j]
+
+
+# In[268]:
+
+
+times = 1000
+updates= np.empty(times)
+w0 = np.empty(times)
+
+for i in range(times):
+    w = np.zeros(11)
+    correct_counter = 0
+    updates_counter = 0
+    while(correct_counter < 5 * N):
+        t = rdm.randint(0,N - 1)
+        xt = x[t,:]
+        wx = np.inner(w,xt)
+        if sign(wx) != ans_y[t]:
+            w = w + ans_y[t] * xt
+            correct_counter = 0
+            updates_counter += 1
+        else:
+            correct_counter += 1
+    
+    wPLA = w
+    updates[i] = updates_counter
+    w0[i] = wPLA[0]
+    
+ans_18 = median(updates)
+print("The median number of updates =", ans_18)
+
+
+# In[ ]:
+
+
+
+
---
+++ 2020_Fall/ML_Foundation and Techniques/hw1/hw1_20.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[255]:
+
+
+import pandas as pd
+import numpy as np
+import random as rdm
+from statistics import median
+
+
+# In[256]:
+
+
+mydata = pd.read_csv("https://www.csie.ntu.edu.tw/~htlin/course/ml20fall/hw1/hw1_train.dat" , sep="\s+", header = None)
+
+
+# In[257]:
+
+
+def sign(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
+
+
+# In[271]:
+
+
+N = 100
+data = mydata.to_numpy()
+x = np.zeros([N,11])
+ans_y = np.empty(N)
+for i in range(N):
+    ans_y[i] = data[i,10]
+    for j in range(10):
+        x[i,j+1] = data[i,j]
+x = x * 0.25
+
+
+# In[272]:
+
+
+times = 1000
+updates= np.empty(times)
+w0 = np.empty(times)
+
+for i in range(times):
+    w = np.zeros(11)
+    correct_counter = 0
+    updates_counter = 0
+    while(correct_counter < 5 * N):
+        t = rdm.randint(0,N - 1)
+        xt = x[t,:]
+        wx = np.inner(w,xt)
+        if sign(wx) != ans_y[t]:
+            w = w + ans_y[t] * xt
+            correct_counter = 0
+            updates_counter += 1
+        else:
+            correct_counter += 1
+    
+    wPLA = w
+    updates[i] = updates_counter
+    w0[i] = wPLA[0]
+    
+ans_20 = median(updates)
+print("The median number of updates =", ans_20)
+
+
+# In[ ]:
+
+
+
+
